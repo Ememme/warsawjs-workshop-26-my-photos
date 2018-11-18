1bb8a4e8ad043db5302966f687565f4386b488ba
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-11-18 14:01:34 +0100

    Refactor to object

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 const images = document.getElementById('images');
-images.addEventListener('click', ({ target }) => {
-
-  target.classList.toggle('image--favourite');
-  // if (target.classList.contains('image--favourite')) {
-  //   target.classList.remove('image--favourite');
-  // } else {
-  //   target.classList.add('image--favourite');
-  // }
-});
 const imagePath = ['images/DSC05386.JPG', 'images/DSC05389.JPG', 'images/DSC05396.JPG'];
 
+class Image {
+  constructor(path) {
+    this.path = path;
+  }
 
-// imagePath.forEach((image) => {
-//   const newImage = document.createElement('img');
-//   newImage.setAttribute('src', image);
-//   newImage.addEventListener('click', () => {
-//     if (newImage.classList.contains('image--favourite')) {
-//       newImage.classList.remove('image--favourite');
-//     } else {
-//       newImage.classList.add('image--favourite');
-//     }
-//   });
-//   images.appendChild(newImage);
-// });
+  // creating image
+  show() {
+    const image = document.createElement('img');
+    image.setAttribute('src', this.path);
+    image.addEventListener('click', () => this.markOrUnmarkAsFavourite());
+    images.appendChild(image);
+    this.imageElement = image;
+  }
 
+  // toggle favourites
+  markOrUnmarkAsFavourite() {
+    this.imageElement.classList.toggle('image--favourite');
+  }
+}
 
 imagePath.forEach((image) => {
-  const newImage = document.createElement('img');
-  newImage.setAttribute('src', image);
-  // newImage.addEventListener('click', () => newImage.classList.toggle('image--favourite'));
-  images.appendChild(newImage);
+  const newImage = new Image(image)
+  newImage.show();
 });
