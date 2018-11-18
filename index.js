02f677ce4dc426edc6d51204a9c5484ece9c6476
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-11-18 11:29:57 +0100

    Refactor to ES6

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const images = document.getElementById('images');
 const imagePath = ['images/DSC05386.JPG', 'images/DSC05389.JPG', 'images/DSC05396.JPG'];
 
 
-imagePath.forEach(function(image){
+// imagePath.forEach(function(image){
+//   const newImage = document.createElement('img');
+//   // debugger;
+//   newImage.setAttribute('src', image);
+//   images.appendChild(newImage);
+// });
+
+imagePath.forEach((image) => {
   const newImage = document.createElement('img');
-  // debugger;
   newImage.setAttribute('src', image);
   images.appendChild(newImage);
-})
+});
