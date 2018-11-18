7b35538fbf695d9296750a7914b8012db7aa1154
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-11-18 12:32:24 +0100

    Change eventListener to document instead of loop

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const images = document.getElementById('images');
+images.addEventListener('click', function(event) {
+  let img = event.target;
+  console.log('I was clicked');
+  // event.target.classList.toggle('image--favourite');
+  if (img.classList.contains('image--favourite')) {
+    img.classList.remove('image--favourite');
+  } else {
+    img.classList.add('image--favourite');
+  }
+});
 const imagePath = ['images/DSC05386.JPG', 'images/DSC05389.JPG', 'images/DSC05396.JPG'];
 
 
-// imagePath.forEach(function(image){
+// imagePath.forEach((image) => {
 //   const newImage = document.createElement('img');
-//   // debugger;
 //   newImage.setAttribute('src', image);
+//   newImage.addEventListener('click', () => {
+//     if (newImage.classList.contains('image--favourite')) {
+//       newImage.classList.remove('image--favourite');
+//     } else {
+//       newImage.classList.add('image--favourite');
+//     }
+//   });
 //   images.appendChild(newImage);
 // });
+
 
 imagePath.forEach((image) => {
   const newImage = document.createElement('img');
   newImage.setAttribute('src', image);
-  newImage.addEventListener('click', () => {
-    if (newImage.classList.contains('image--favourite')) {
-      newImage.classList.remove('image--favourite');
-    } else {
-      newImage.classList.add('image--favourite');
-    }
-  });
+  // newImage.addEventListener('click', () => newImage.classList.toggle('image--favourite'));
   images.appendChild(newImage);
 });
